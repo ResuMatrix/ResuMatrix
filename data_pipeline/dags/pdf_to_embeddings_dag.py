35953936429beb9aa360342fadb7529dce2059a2
Shailendra Tripathi <tripathi.shai@northeastern.edu>
2025-03-02 18:46:17 -0500

    DAG changes to incorporate data pipeline.

--- data_pipeline/dags/pdf_to_embeddings_dag.py
+++ data_pipeline/dags/pdf_to_embeddings_dag.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator 
 from airflow.operators.empty import EmptyOperator
+from airflow.operators.email import EmailOperator
 import os
 
 default_args = {
@@ -18,46 +19,104 @@ os.makedirs(DATA_DIR, exist_ok=True)
 
 # Pipeline
 
+def load_resumes():
+    print("Loading dataset for resume classification.")
+    print("Successfully loaded resume classification dataset!")
+
+
 def extract_text_from_pdf():
-    print("Extracting text from pdf")
+    print("Extracting text from pdf.")
 
 
-def preprocess():
-    print("Processing text")
+def data_cleaning():
+    print("Cleaning data and removing personal information.")
 
 
 def generate_embeddings():
-    print("Generating Embeddings")
-    print("Embeddings generated successfuly!")
+    print("Generating Embeddings.")
+    print("Embeddings generated successfully!")
 
 
-with DAG(
+def parsing_text_json_schema():
+    print("Parsing text for LLM in JSON schema.")
+    print("Parsed data successfully!")
+
+
+def data_transform():
+    print("Transforming data into necessary format.")
+    print("Successfully transformed data!")
+
+
+with (DAG(
         'pdf_to_embeddings_pipeline',
         default_args=default_args,
         description="A pipeline for extracting text from resumes and creating embeddings"
-        ) as dag:
+        ) as dag):
 
     start_task = EmptyOperator(task_id='start')
 
+    load_task = PythonOperator(
+        task_id="load_resumes_task",
+        python_callable=load_resumes,
+        dag=dag
+    )
+
     extract_task = PythonOperator(
-        task_id= 'sample_extract_text_task',
+        task_id="sample_extract_text_task",
         python_callable=extract_text_from_pdf,
         dag=dag
-        )
+    )
 
-    preprocess_task = PythonOperator(
-        task_id = 'sample_preprocess_text_task',
-        python_callable= preprocess,
+    data_cleaning_task = PythonOperator(
+        task_id="data_cleaning_task",
+        python_callable=data_cleaning,
         dag=dag
-        )
+    )
 
     embed_task = PythonOperator(
-        task_id = 'sample_embed_text_task',
+        task_id="embedding_generation_task",
         python_callable=generate_embeddings,
         dag=dag
-        )
+    )
+
+    parse_text_json_task = PythonOperator(
+        task_id="parse_text_json_task",
+        python_callable=parsing_text_json_schema,
+        dag=dag
+    )
+
+    data_transformation_task = PythonOperator(
+        task_id="data_transformation_task",
+        python_callable=data_transform,
+        dag=dag
+    )
+
     end_task = EmptyOperator(task_id='end')
 
+    email_success = EmailOperator(
+        task_id='send_email_success',
+        to='mlops.team20@gmail.com',
+        subject='Airflow Task Success: {{ task_instance.task_id }}',
+        html_content="""
+        <h3>Task {{ task_instance.task_id }} has completed successfully.</h3>
+        <p>DAG: {{ dag.dag_id }}</p>
+        """,
+        trigger_rule='all_success',
+        dag=dag
+    )
+
+    email_failure = EmailOperator(
+        task_id='send_email_failure',
+        to='mlops.team20@gmail.com',
+        subject='Airflow Task Failed: {{ task_instance.task_id }}',
+        html_content="""
+        <h3>Task {{ task_instance.task_id }} has failed.</h3>
+        <p>DAG: {{ dag.dag_id }}</p>
+        """,
+        trigger_rule='one_failed',  # Triggers only if a task fails
+        dag=dag
+    )
+
     # set the task dependencies
-    start_task >> extract_task >> preprocess_task >> embed_task >> end_task
+    start_task >> load_task >> extract_task >> data_cleaning_task >> [embed_task, parse_text_json_task] >> data_transformation_task >> end_task >> [email_success, email_failure]
 
