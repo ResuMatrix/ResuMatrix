9be9bb78a9b1dfa82cf7d2a710fc22c3c38ac986
Nishchith Rao <nishchith14600@gmail.com>
2025-03-02 21:21:34 -0500

    docker and dag smtp and logging changes.

--- data_pipeline/dags/pdf_to_embeddings_dag.py
+++ data_pipeline/dags/pdf_to_embeddings_dag.py
@@ -3,13 +3,20 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator 
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.email import EmailOperator
+from airflow.utils.log.logging_mixin import LoggingMixin
 import os
+
+# Using Airflow's built-in logger
+log = LoggingMixin().log  
 
 default_args = {
         'owner': 'admin',
         'start_date': datetime(2025, 2, 28),
         'retries': 1,
-        'retry_delay': timedelta(minutes=5),
+        'retry_delay': timedelta(minutes=3),
+        'email_on_failure': True,
+        'email_on_success': True,
+        'email': ['mlops.team20@gmail.com'],
         }
 
 DATA_DIR = '~/data/workspace/mlops/ResuMatrix/data'
@@ -20,35 +27,45 @@ os.makedirs(DATA_DIR, exist_ok=True)
 # Pipeline
 
 def load_resumes():
+    log.info("Loading dataset for resume classification.")
     print("Loading dataset for resume classification.")
+    log.info("Successfully loaded resume classification dataset!")
     print("Successfully loaded resume classification dataset!")
 
 
 def extract_text_from_pdf():
+    log.info("Extracting text from pdf.")
     print("Extracting text from pdf.")
 
 
 def data_cleaning():
+    log.info("Cleaning data and removing personal information.")
     print("Cleaning data and removing personal information.")
 
 
 def generate_embeddings():
+    log.info("Generating Embeddings.")
     print("Generating Embeddings.")
+    log.info("Embeddings generated successfully!")
     print("Embeddings generated successfully!")
 
 
 def parsing_text_json_schema():
+    log.info("Parsing text for LLM in JSON schema.")
     print("Parsing text for LLM in JSON schema.")
+    log.info("Parsed data successfully!")
     print("Parsed data successfully!")
 
-
 def data_transform():
+    log.info("Transforming data into necessary format.")
     print("Transforming data into necessary format.")
+
+    log.info("Successfully transformed data!")
     print("Successfully transformed data!")
 
 
 with (DAG(
-        'pdf_to_embeddings_pipeline',
+        'resumatrix_data_pipeline_dag',
         default_args=default_args,
         description="A pipeline for extracting text from resumes and creating embeddings"
         ) as dag):
