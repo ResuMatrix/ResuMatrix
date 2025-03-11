aba6f1e9178b488c425bdbd893b52372666f360e
Nishchith Rao <nishchith14600@gmail.com>
2025-03-11 16:00:48 -0400

    testing jenkins automation

--- src/test_code.py
+++ src/test_code.py
@@ -16,12 +16,12 @@ logging.basicConfig(
 
 # Test script logic
 def main():
-    logging.info("====== Starting Test Code ======")
+    logging.info("====== Starting Automatio Test Code ======")
     print("Running the test script...")
     for i in range(5):
         logging.info(f"Processing step {i+1}")
         print(f"Step {i+1} complete.")
-    logging.info("====== Test Code Finished ======")
+    logging.info("====== Automatio Test Code Finished ======")
 
 if __name__ == "__main__":
     main()
