553d2c45284a25f0bd70c85db11aacccfb5e7afc
Shailendra Tripathi <tripathi.shai@northeastern.edu>
2025-03-10 23:56:54 -0400

    loading data function.

--- src/data_processing/data_preprocessing.py
+++ src/data_processing/data_preprocessing.py
@@ -1,6 +1,17 @@
 import pandas as pd
 import os
 
-print(os.getcwd())
 
-# train_df = pd.read_csv("../model_training_data/resume_job_description_fit/train.csv")
+def load_data(data_type="train"):
+    current_file_path = os.path.abspath(__file__)
+    if data_type not in ["train", "test"]:
+        raise ValueError("Expecting data_type to be train or test.")
+
+    parent_dir = current_file_path[:current_file_path.index("ResuMatrix") + 10]
+    src_dir = os.path.join(parent_dir, "src")
+
+    data_file_path = os.path.join(src_dir, "model_training_data", "resume_job_description_fit", data_type + ".csv")
+    return pd.read_csv(data_file_path)
+
+
+
